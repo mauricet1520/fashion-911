9ec1915e38ebf82bc0484acf98f9570412e15d85
Maurice Thomas <mauricet1520@gmail.com>
2017-03-17 15:51:04 -0400

    third commit

--- src/main/java/com/r/a/project/ClientController.java
+++ src/main/java/com/r/a/project/ClientController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by crci1 on 3/17/2017.
@@ -25,9 +27,21 @@ public class ClientController {
 
         client = new Client(client.getFirstName(), client.getLastName(), client.getAddress(), client.getEmail(), client.getPhoneNumber(),
                 client.getHeight(), client.getSize(), client.getBodyType(), client.getEventType(), client.getColorsLiked(), client.getColorsDisliked(),
-                client.getZipCode(), client.getCity(), client.getState(),java.sql.Timestamp.valueOf(LocalDateTime.of(2017, Month.from(Month.APRIL), 28, 1, 5)), client.getImage());
+                client.getZipCode(), client.getCity(), client.getState(),java.sql.Timestamp.valueOf(LocalDateTime.of(2017, Month.from(Month.APRIL), 2, 1, 5)), client.getImage());
 
         clients.save(client);
         return client;
+    }
+
+    @RequestMapping(path = "get_clients.json", method = RequestMethod.GET)
+    public List<Client> getClients() {
+        List<Client> theClients = new ArrayList<>();
+
+        Iterable<Client> clientList = clients.findAll();
+        for (Client list : clientList) {
+            theClients.add(list);
+        }
+        return theClients;
+
     }
 }
